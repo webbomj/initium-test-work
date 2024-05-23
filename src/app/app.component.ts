ff64a379991886943be0f2c38b913f5de43242ec
webbomj <nethack@mail.ru>
2024-05-23 13:40:26 +0300

    feat: add fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  // template: '<router-outlet></router-outlet>',
+  template: '<p>Alice</p>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
