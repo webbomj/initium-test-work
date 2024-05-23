b2388fe975345918f0b61c5112fd4471d768822c
webbomj <nethack@mail.ru>
2024-05-23 13:44:25 +0300

    feat: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // template: '<router-outlet></router-outlet>',
-  template: '<p>Alice</p>',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ src/app/pages/clients/clients.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.scss'],
+  standalone: true
+})
+export class ClientsComponent {
+
+}
